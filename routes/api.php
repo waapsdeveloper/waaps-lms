633ab5ca927fb63c34b39397465b2701ab880a59
abdullah hanif <99804964+waapsdeveloper@users.noreply.github.com>
2023-11-12 09:56:00 +0500

    user API

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\RoleController;
+use App\Http\Controllers\API\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -21,26 +22,40 @@ use App\Http\Controllers\API\RoleController;
 
 Route::group(['prefix' => 'roles'], function () {
     // Index - List all roles
-    Route::get('/', 'RoleController@index');
+    Route::get('/', [RoleController::class, 'index']);
 
     // Store - Store a newly created role in the database
-    Route::post('/', 'RoleController@store');
+    Route::post('/', [RoleController::class, 'store']);
 
     // Update - Update the specified role in the database
-    Route::put('/{role}', 'RoleController@update');
+    Route::put('/{role}', [RoleController::class, 'update']);
 
     // Destroy - Remove the specified role from the database
-    Route::delete('/{role}', 'RoleController@destroy');
+    Route::delete('/{role}', [RoleController::class, 'destroy']);
+
+});
+
+Route::group(['prefix' => 'users'], function () {
+    // Index - List all users
+    Route::get('/', [UserController::class, 'index']);
+
+    // Store - Store a newly created user in the database
+    Route::post('/', [UserController::class, 'store']);
+
+    // Update - Update the specified user in the database
+    Route::put('/{user}', [UserController::class, 'update']);
+
+    // Destroy - Remove the specified user from the database
+    Route::delete('/{user}', [UserController::class, 'destroy']);
 });
 
 
 
+// Route::post('/register', 'App\Http\Controllers\API\AuthController@register');
 
-Route::post('/register', 'App\Http\Controllers\API\AuthController@register');
+// Route::get('/users', 'App\Http\Controllers\API\AuthController@getAllUsers');
 
-Route::get('/users', 'App\Http\Controllers\API\AuthController@getAllUsers');
+// Route::post('/update/{userid}', 'App\Http\Controllers\API\AuthController@update');
 
-Route::post('/update/{userid}', 'App\Http\Controllers\API\AuthController@update');
-
-Route::post('/delete/{userid}', 'App\Http\Controllers\API\AuthController@delete');
+// Route::post('/delete/{userid}', 'App\Http\Controllers\API\AuthController@delete');
 
