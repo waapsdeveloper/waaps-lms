03fd9dcf43c90cb94914a17746799158ab2e5a74
abdullah hanif <99804964+waapsdeveloper@users.noreply.github.com>
2023-11-24 12:15:33 +0500

    small changes

--- resources/js/pages/onboarding.vue
+++ resources/js/pages/onboarding.vue
@@ -1,163 +1,247 @@
 <template>
-    <!-- Categorry Section Starts -->
-    <section class="category-section">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="section-title text-center">
-                        <h2>Tell us your <span>Interests</span></h2>
-                        <p>This will help both of us start further</p>
-                    </div>
-                </div>
-                <div class="col-12">
-                    <div>
-
-                        Select You Base Momentum,
-
-                        <div class="">
-
-                        </div>
-
-
-                    </div>
-                </div>
-
-
-
-
-
-
+  <!-- Categorry Section Starts -->
+  <section class="course-category-section padding-top-120 padding-bottom-90">
+    <div class="container">
+      <div class="row">
+          <div class="col-lg-4">
+            <h3 class="my-3">My Prefrences</h3>
+          <div class="course-category-sidebar">
+            <div class="lms-single-widget">
+              <div class="lms-widget-title">
+                <h4>Platform</h4>
+              </div>
+              <ul>
+                <li class="">
+                  <a href="#">web development</a>
+                </li>
+                <li class="">
+                  <a href="#">Mobile development</a>
+                </li>
+              </ul>
             </div>
-            <div class="row">
-                <div class="col-lg-3 col-md-6">
-                    <a href="/course">
-                        <div class="single-category-item">
-                            <div class="category-image">
-                                <img src="assets/images/category-icon-1.png" alt="image">
-                                <img src="assets/images/round-shape-3.png" alt="shape" class="feature-round-shape-3">
-                            </div>
-                            <div class="category-title margin-bottom-10">
-                                <h4>web design</h4>
-                            </div>
-                            <span>03 course(S)</span>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <a href="/course">
-                        <div class="single-category-item">
-                            <div class="category-image">
-                                <img src="assets/images/category-icon-2.png" alt="image">
-                                <img src="assets/images/round-shape-3.png" alt="shape" class="feature-round-shape-3">
-                            </div>
-                            <div class="category-title margin-bottom-10">
-                                <h4>marketing</h4>
-                            </div>
-                            <span>02 course(S)</span>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <a href="/course">
-                        <div class="single-category-item">
-                            <div class="category-image">
-                                <img src="assets/images/category-icon-3.png" alt="image">
-                                <img src="assets/images/round-shape-3.png" alt="shape" class="feature-round-shape-3">
-                            </div>
-                            <div class="category-title margin-bottom-10">
-                                <h4>education</h4>
-                            </div>
-                            <span>10 course(S)</span>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <a href="/course">
-                        <div class="single-category-item">
-                            <div class="category-image">
-                                <img src="assets/images/category-icon-4.png" alt="image">
-                                <img src="assets/images/round-shape-3.png" alt="shape" class="feature-round-shape-3">
-                            </div>
-                            <div class="category-title margin-bottom-10">
-                                <h4>business</h4>
-                            </div>
-                            <span>04 course(S)</span>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <a href="/course">
-                        <div class="single-category-item">
-                            <div class="category-image">
-                                <img src="assets/images/category-icon-5.png" alt="image">
-                                <img src="assets/images/round-shape-3.png" alt="shape" class="feature-round-shape-3">
-                            </div>
-                            <div class="category-title margin-bottom-10">
-                                <h4>psychology</h4>
-                            </div>
-                            <span>03 course(S)</span>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <a href="/course">
-                        <div class="single-category-item">
-                            <div class="category-image">
-                                <img src="assets/images/category-icon-6.png" alt="image">
-                                <img src="assets/images/round-shape-3.png" alt="shape" class="feature-round-shape-3">
-                            </div>
-                            <div class="category-title margin-bottom-10">
-                                <h4>UI & UX design</h4>
-                            </div>
-                            <span>05 course(S)</span>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <a href="/course">
-                        <div class="single-category-item">
-                            <div class="category-image">
-                                <img src="assets/images/category-icon-7.png" alt="image">
-                                <img src="assets/images/round-shape-3.png" alt="shape" class="feature-round-shape-3">
-                            </div>
-                            <div class="category-title margin-bottom-10">
-                                <h4>technology</h4>
-                            </div>
-                            <span>09 course(S)</span>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <a href="/course">
-                        <div class="single-category-item">
-                            <div class="category-image">
-                                <img src="assets/images/category-icon-8.png" alt="image">
-                                <img src="assets/images/round-shape-3.png" alt="shape" class="feature-round-shape-3">
-                            </div>
-                            <div class="category-title margin-bottom-10">
-                                <h4>photography</h4>
-                            </div>
-                            <span>08 course(S)</span>
-                        </div>
-                    </a>
-                </div>
+            <div class="lms-single-widget">
+              <div class="lms-widget-title">
+                <h4>web Development</h4>
+              </div>
+              <ul>
+                <li class=""><a href="#">Type Script</a></li>
+                <li><a href="#">NodeJS</a></li>
+                <li><a href="#">React</a></li>
+                <li><a href="#">Angular</a></li>
+                <li><a href="#">VueJs</a></li>
+                <li><a href="#">Laravel</a></li>
+                <li><a href="#">PHP</a></li>
+              </ul>
             </div>
+
+            <div class="lms-single-widget">
+              <div class="lms-widget-title">
+                <h4>Mobile Development</h4>
+              </div>
+              <ul>
+                <li class=""><a href="#">Angular</a></li>
+                <li><a href="#">Ionic</a></li>
+                <li><a href="#">JAVA</a></li>
+                <li><a href="#">Swift</a></li>
+                <li><a href="#">React Native</a></li>
+              </ul>
+            </div>
+          </div>
         </div>
-    </section>
+        <div class="col-lg-8">
+            <form action="">
+                <h4 class="text-center">Hands on your experience</h4>
+                <h5 class="my-3">Select your current experience:</h5>
+                <span class="badge-style">
+                    <span class="badge  badge-style" >Type Script</span>
+                    <span class="badge  badge-style">NodeJS</span>
+                    <span class="badge  badge-style">React</span>
+                    <span class="badge  badge-style">Angular</span>
+                    <span class="badge  badge-style">VueJS</span>
+                    <span class="badge  badge-style">Laravel</span>
+                    <span class="badge  badge-style">PHP</span>
+                    <span class="badge  badge-style">Ionic</span>
+                    <span class="badge  badge-style">JAVA</span>
+                    <span class="badge  badge-style">Swift</span>
+                    <span class="badge  badge-style">React Native</span>
+                </span>
+            </form>
+          <div class="row">
+            <div class="col-md-6">
+              <a href="/course">
+                <div class="single-category-item">
+                  <div class="category-image">
+                    <img src="assets/images/category-icon-1.png" alt="image" />
+                    <img
+                      src="assets/images/round-shape-3.png"
+                      alt="shape"
+                      class="feature-round-shape-3"
+                    />
+                  </div>
+                  <div class="category-title margin-bottom-10">
+                    <h4>web design</h4>
+                  </div>
+                  <span>03 course(S)</span>
+                </div>
+              </a>
+            </div>
+            <div class="col-md-6">
+              <a href="/course">
+                <div class="single-category-item">
+                  <div class="category-image">
+                    <img src="assets/images/category-icon-2.png" alt="image" />
+                    <img
+                      src="assets/images/round-shape-3.png"
+                      alt="shape"
+                      class="feature-round-shape-3"
+                    />
+                  </div>
+                  <div class="category-title margin-bottom-10">
+                    <h4>marketing</h4>
+                  </div>
+                  <span>02 course(S)</span>
+                </div>
+              </a>
+            </div>
+            <div class="col-md-6">
+              <a href="/course">
+                <div class="single-category-item">
+                  <div class="category-image">
+                    <img src="assets/images/category-icon-3.png" alt="image" />
+                    <img
+                      src="assets/images/round-shape-3.png"
+                      alt="shape"
+                      class="feature-round-shape-3"
+                    />
+                  </div>
+                  <div class="category-title margin-bottom-10">
+                    <h4>education</h4>
+                  </div>
+                  <span>10 course(S)</span>
+                </div>
+              </a>
+            </div>
+            <div class="col-md-6">
+              <a href="/course">
+                <div class="single-category-item">
+                  <div class="category-image">
+                    <img src="assets/images/category-icon-4.png" alt="image" />
+                    <img
+                      src="assets/images/round-shape-3.png"
+                      alt="shape"
+                      class="feature-round-shape-3"
+                    />
+                  </div>
+                  <div class="category-title margin-bottom-10">
+                    <h4>business</h4>
+                  </div>
+                  <span>04 course(S)</span>
+                </div>
+              </a>
+            </div>
+            <div class="col-md-6">
+              <a href="/course">
+                <div class="single-category-item">
+                  <div class="category-image">
+                    <img src="assets/images/category-icon-5.png" alt="image" />
+                    <img
+                      src="assets/images/round-shape-3.png"
+                      alt="shape"
+                      class="feature-round-shape-3"
+                    />
+                  </div>
+                  <div class="category-title margin-bottom-10">
+                    <h4>psychology</h4>
+                  </div>
+                  <span>03 course(S)</span>
+                </div>
+              </a>
+            </div>
+            <div class="col-md-6">
+              <a href="/course">
+                <div class="single-category-item">
+                  <div class="category-image">
+                    <img src="assets/images/category-icon-6.png" alt="image" />
+                    <img
+                      src="assets/images/round-shape-3.png"
+                      alt="shape"
+                      class="feature-round-shape-3"
+                    />
+                  </div>
+                  <div class="category-title margin-bottom-10">
+                    <h4>UI & UX design</h4>
+                  </div>
+                  <span>05 course(S)</span>
+                </div>
+              </a>
+            </div>
+
+            <div class="col-md-6">
+              <a href="/course">
+                <div class="single-category-item">
+                  <div class="category-image">
+                    <img src="assets/images/category-icon-7.png" alt="image" />
+                    <img
+                      src="assets/images/round-shape-3.png"
+                      alt="shape"
+                      class="feature-round-shape-3"
+                    />
+                  </div>
+                  <div class="category-title margin-bottom-10">
+                    <h4>technology</h4>
+                  </div>
+                  <span>09 course(S)</span>
+                </div>
+              </a>
+            </div>
+
+            <div class="col-md-6">
+              <a href="/course">
+                <div class="single-category-item">
+                  <div class="category-image">
+                    <img src="assets/images/category-icon-8.png" alt="image" />
+                    <img
+                      src="assets/images/round-shape-3.png"
+                      alt="shape"
+                      class="feature-round-shape-3"
+                    />
+                  </div>
+                  <div class="category-title margin-bottom-10">
+                    <h4>photography</h4>
+                  </div>
+                  <span>08 course(S)</span>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-import Base from './base.vue';
+import Base from "./base.vue";
 
 export default {
-    extends: Base,
-    name: 'onboarding',
-    mounted() {
-
-    }
-
-
-
-}
+  extends: Base,
+  name: "onboarding",
+  mounted() {},
+};
 </script>
-<style></style>
+<style>
+.badge-style {
+ margin-left:20px;
+ margin-right:20px;
+ width: 100px;
+ font-size: 15px;
+ border-radius: 20px;
+ padding-top: 8px;
+ padding-bottom: 8px;
+ margin: ;
+}
+.badge{
+    background-color: #50be46;
+    cursor: pointer;
+}
+</style>
