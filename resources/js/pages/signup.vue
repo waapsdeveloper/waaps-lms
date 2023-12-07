23fef874c1866a7a00066a80e0cafb05d583158f
Waapsdeveloper <waapsdeveloper@gmail.com>
2023-12-07 16:39:43 +0500

    signup continue

--- resources/js/pages/signup.vue
+++ resources/js/pages/signup.vue
@@ -1,122 +1,189 @@
 <template>
-<div class="d-flex flex-column flex-column-fluid flex-lg-row"  style="background-image: url('assets/media/auth/bg4-dark.jpg')">
-				<!--begin::Aside-->
-				<div class="d-flex flex-center w-lg-50 pt-15 pt-lg-0 px-10">
-					<!--begin::Aside-->
-					<div class="d-flex flex-column">
-						<!--begin::Logo-->
-						<a href="../../demo1/dist/index.html" class="mb-7">
-							<img alt="Logo" src="/assets/media/logos/custom-3.svg" />
-						</a>
-						<!--end::Logo-->
-						<!--begin::Title-->
-						<h2 class="text-white fw-normal m-0">Branding tools designed for your business</h2>
-						<!--end::Title-->
-					</div>
-					<!--begin::Aside-->
-				</div>
-				<!--begin::Aside-->
-				<!--begin::Body-->
-				<div class="d-flex flex-center w-lg-50 p-10">
-					<!--begin::Card-->
-					<div class="card rounded-3 w-md-550px">
-						<!--begin::Card body-->
-						<div class="card-body p-10 p-lg-20">
-							<!--begin::Form-->
-							<form class="form w-100" novalidate="novalidate" id="kt_sign_up_form">
-								<!--begin::Heading-->
-								<div class="text-center mb-11">
-									<!--begin::Title-->
-									<h1 class="text-dark fw-bolder mb-3">Sign Up</h1>
-									<!--end::Title-->
-									<!--begin::Subtitle-->
-									<div class="text-gray-500 fw-semibold fs-6">Your Social Campaigns</div>
-									<!--end::Subtitle=-->
-								</div>
-								<!--begin::Heading-->
-								<!--begin::Login options-->
-								<div class="row g-3 mb-9">
-								</div>
-                                <div class="fv-row mb-8">
-									<!--begin::Email-->
-									<input type="text" placeholder="Name" name="name" autocomplete="off" class="form-control bg-transparent" v-model="auth.name" />
-									<!--end::Email-->
-								</div>
-								<div class="fv-row mb-8">
-									<!--begin::Email-->
-									<input type="text" placeholder="Email" name="email" autocomplete="off" class="form-control bg-transparent" v-model="auth.email" />
-									<!--end::Email-->
-								</div>
-								<!--begin::Input group-->
-								<div class="fv-row mb-8" data-kt-password-meter="true">
-									<!--begin::Wrapper-->
-									<div class="mb-1">
-										<!--begin::Input wrapper-->
-										<div class="position-relative mb-3">
-											<input class="form-control bg-transparent" type="password" placeholder="Password" name="password" autocomplete="off" v-model="auth.password" />
-											<span class="btn btn-sm btn-icon position-absolute translate-middle top-50 end-0 me-n2" data-kt-password-meter-control="visibility">
-												<i class="bi bi-eye-slash fs-2"></i>
-												<i class="bi bi-eye fs-2 d-none"></i>
-											</span>
-										</div>
-										<!--end::Input wrapper-->
-										<!--begin::Meter-->
-										<div class="d-flex align-items-center mb-3" data-kt-password-meter-control="highlight">
-											<div class="flex-grow-1 bg-secondary bg-active-success rounded h-5px me-2"></div>
-											<div class="flex-grow-1 bg-secondary bg-active-success rounded h-5px me-2"></div>
-											<div class="flex-grow-1 bg-secondary bg-active-success rounded h-5px me-2"></div>
-											<div class="flex-grow-1 bg-secondary bg-active-success rounded h-5px"></div>
-										</div>
-										<!--end::Meter-->
-									</div>
-									<!--end::Wrapper-->
-									<!--begin::Hint-->
-									<div class="text-muted">Use 8 or more characters with a mix of letters, numbers &amp; symbols.</div>
-									<!--end::Hint-->
-								</div>
-								<!--end::Input group=-->
-								<!--end::Input group=-->
-								<div class="fv-row mb-8">
-									<!--begin::Repeat Password-->
-									<input placeholder="Confirm Password" name="confirm-password" type="password" autocomplete="off" class="form-control bg-transparent" v-model="auth.confirm_password" />
-									<!--end::Repeat Password-->
-								</div>
-								<!--end::Input group=-->
-								<!--begin::Accept-->
-								<div class="fv-row mb-8">
-									<label class="form-check form-check-inline">
-										<input class="form-check-input" type="checkbox" name="toc" value="1" />
-										<span class="form-check-label fw-semibold text-gray-700 fs-base ms-1">I Accept the
-										<a href="#" class="ms-1 link-primary">Terms</a></span>
-									</label>
-								</div>
-								<!--end::Accept-->
-								<!--begin::Submit button-->
-								<div class="d-grid mb-10">
-									<button type="submit" id="kt_sign_up_submit" class="btn btn-primary">
-										<!--begin::Indicator label-->
-										<span class="indicator-label" @click="validate" >Sign up</span>
-										<!--end::Indicator label-->
-										<!--begin::Indicator progress-->
-										<span class="indicator-progress">Please wait...
-										<span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
-										<!--end::Indicator progress-->
-									</button>
-								</div>
-								<!--end::Submit button-->
-								<!--begin::Sign up-->
-								<div class="text-gray-500 text-center fw-semibold fs-6">Already have an Account?
-								<a @click="route('/login')" class="link-primary fw-semibold" style="cursor: pointer;">Sign in</a></div>
-								<!--end::Sign up-->
-							</form>
-							<!--end::Form-->
-						</div>
-						<!--end::Card body-->
-					</div>
-					<!--end::Card-->
-				</div>
-				<!--end::Body-->
-			</div>
+    <div class="d-flex flex-column flex-column-fluid flex-lg-row"
+        style="background-image: url('assets/media/auth/bg4-dark.jpg')">
+        <!--begin::Aside-->
+        <div class="d-flex flex-center w-lg-50 pt-15 pt-lg-0 px-10">
+            <!--begin::Aside-->
+            <div class="d-flex flex-column">
+                <!--begin::Logo-->
+                <a href="../../demo1/dist/index.html" class="mb-7">
+                    <img alt="Logo" src="/assets/media/logos/custom-3.svg" />
+                </a>
+                <!--end::Logo-->
+                <!--begin::Title-->
+                <h2 class="text-white fw-normal m-0">Branding tools designed for your business</h2>
+                <!--end::Title-->
+            </div>
+            <!--begin::Aside-->
+        </div>
+        <!--begin::Aside-->
+        <!--begin::Body-->
+        <div class="d-flex flex-center w-lg-50 p-10">
+            <!--begin::Card-->
+            <div class="card rounded-3 w-md-550px">
+                <!--begin::Card body-->
+                <div class="card-body p-10 p-lg-20">
+                    <!--begin::Form-->
+                    <form class="form w-100" novalidate="novalidate" id="kt_sign_up_form">
+                        <!--begin::Heading-->
+                        <div class="text-center mb-11">
+                            <!--begin::Title-->
+                            <h1 class="text-dark fw-bolder mb-3">Sign Up</h1>
+                            <!--end::Title-->
+                            <!--begin::Subtitle-->
+                            <div class="text-gray-500 fw-semibold fs-6">Your Social Campaigns</div>
+                            <!--end::Subtitle=-->
+                        </div>
+                        <div class="fv-row mb-10">
+                            <!--begin::Label-->
+                            <label class="fs-6 fw-semibold mb-2">As a:
+                                <i class="fas fa-exclamation-circle ms-2 fs-7" data-bs-toggle="tooltip"
+                                    title="Show your ads to either men or women, or select 'All' for both"></i></label>
+                            <!--End::Label-->
+                            <!--begin::Row-->
+                            <div class="row g-9" data-kt-buttons="true" data-kt-buttons-target="[data-kt-button='true']">
+                                <!--begin::Col-->
+                                <div class="col">
+                                    <!--begin::Option-->
+                                    <label
+                                        class="btn btn-outline btn-outline-dashed btn-active-light-primary active d-flex text-start p-6"
+                                        data-kt-button="true">
+                                        <!--begin::Radio-->
+                                        <span
+                                            class="form-check form-check-custom form-check-solid form-check-sm align-items-start mt-1">
+                                            <input class="form-check-input" type="radio" name="student" value="student"
+                                                  v-model="auth.role" />
+                                        </span>
+                                        <!--end::Radio-->
+                                        <!--begin::Info-->
+                                        <span class="ms-5">
+                                            <span class="fs-4 fw-bold text-gray-800 d-block">Student</span>
+                                        </span>
+                                        <!--end::Info-->
+                                    </label>
+                                    <!--end::Option-->
+                                </div>
+                                <!--end::Col-->
+                                <!--begin::Col-->
+                                <div class="col">
+                                    <!--begin::Option-->
+                                    <label
+                                        class="btn btn-outline btn-outline-dashed btn-active-light-primary d-flex text-start p-6"
+                                        data-kt-button="true">
+                                        <!--begin::Radio-->
+                                        <span
+                                            class="form-check form-check-custom form-check-solid form-check-sm align-items-start mt-1">
+                                            <input class="form-check-input" type="radio" name="instructor" value="instructor"  v-model="auth.role" />
+                                        </span>
+                                        <!--end::Radio-->
+                                        <!--begin::Info-->
+                                        <span class="ms-5">
+                                            <span class="fs-4 fw-bold text-gray-800 d-block">Instructor</span>
+                                        </span>
+                                        <!--end::Info-->
+                                    </label>
+                                    <!--end::Option-->
+                                </div>
+                                <!--end::Col-->
+                                <!--begin::Col-->
+
+                                <!--end::Col-->
+                            </div>
+                            <!--end::Row-->
+                        </div>
+
+                        <!--begin::Heading-->
+                        <!--begin::Login options-->
+                        <div class="row g-3 mb-9">
+                        </div>
+                        <div class="fv-row mb-8">
+                            <!--begin::Email-->
+                            <input type="text" placeholder="Name" name="name" autocomplete="off"
+                                class="form-control bg-transparent" v-model="auth.name" />
+                            <!--end::Email-->
+                        </div>
+                        <div class="fv-row mb-8">
+                            <!--begin::Email-->
+                            <input type="text" placeholder="Email" name="email" autocomplete="off"
+                                class="form-control bg-transparent" v-model="auth.email" />
+                            <!--end::Email-->
+                        </div>
+                        <!--begin::Input group-->
+                        <div class="fv-row mb-8" data-kt-password-meter="true">
+                            <!--begin::Wrapper-->
+                            <div class="mb-1">
+                                <!--begin::Input wrapper-->
+                                <div class="position-relative mb-3">
+                                    <input class="form-control bg-transparent" type="password" placeholder="Password"
+                                        name="password" autocomplete="off" v-model="auth.password" />
+                                    <span class="btn btn-sm btn-icon position-absolute translate-middle top-50 end-0 me-n2"
+                                        data-kt-password-meter-control="visibility">
+                                        <i class="bi bi-eye-slash fs-2"></i>
+                                        <i class="bi bi-eye fs-2 d-none"></i>
+                                    </span>
+                                </div>
+                                <!--end::Input wrapper-->
+                                <!--begin::Meter-->
+                                <div class="d-flex align-items-center mb-3" data-kt-password-meter-control="highlight">
+                                    <div class="flex-grow-1 bg-secondary bg-active-success rounded h-5px me-2"></div>
+                                    <div class="flex-grow-1 bg-secondary bg-active-success rounded h-5px me-2"></div>
+                                    <div class="flex-grow-1 bg-secondary bg-active-success rounded h-5px me-2"></div>
+                                    <div class="flex-grow-1 bg-secondary bg-active-success rounded h-5px"></div>
+                                </div>
+                                <!--end::Meter-->
+                            </div>
+                            <!--end::Wrapper-->
+                            <!--begin::Hint-->
+                            <div class="text-muted">Use 8 or more characters with a mix of letters, numbers &amp; symbols.
+                            </div>
+                            <!--end::Hint-->
+                        </div>
+                        <!--end::Input group=-->
+                        <!--end::Input group=-->
+                        <div class="fv-row mb-8">
+                            <!--begin::Repeat Password-->
+                            <input placeholder="Confirm Password" name="confirm-password" type="password" autocomplete="off"
+                                class="form-control bg-transparent" v-model="auth.confirm_password" />
+                            <!--end::Repeat Password-->
+                        </div>
+                        <!--end::Input group=-->
+                        <!--begin::Accept-->
+                        <div class="fv-row mb-8">
+                            <label class="form-check form-check-inline">
+                                <input class="form-check-input" type="checkbox" name="toc" value="1" />
+                                <span class="form-check-label fw-semibold text-gray-700 fs-base ms-1">I Accept the
+                                    <a href="#" class="ms-1 link-primary">Terms</a></span>
+                            </label>
+                        </div>
+                        <!--end::Accept-->
+                        <!--begin::Submit button-->
+                        <div class="d-grid mb-10">
+                            <button type="submit" id="kt_sign_up_submit" class="btn btn-primary">
+                                <!--begin::Indicator label-->
+                                <span class="indicator-label" @click="validate">Sign up</span>
+                                <!--end::Indicator label-->
+                                <!--begin::Indicator progress-->
+                                <span class="indicator-progress">Please wait...
+                                    <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
+                                <!--end::Indicator progress-->
+                            </button>
+                        </div>
+                        <!--end::Submit button-->
+                        <!--begin::Sign up-->
+                        <div class="text-gray-500 text-center fw-semibold fs-6">Already have an Account?
+                            <a @click="route('/login')" class="link-primary fw-semibold" style="cursor: pointer;">Sign
+                                in</a>
+                        </div>
+                        <!--end::Sign up-->
+                    </form>
+                    <!--end::Form-->
+                </div>
+                <!--end::Card body-->
+            </div>
+            <!--end::Card-->
+        </div>
+        <!--end::Body-->
+    </div>
 </template>
 
 <script>
@@ -145,9 +212,11 @@ export default {
             confirm_password: "",
         });
 
+
         return {
             errors,
             auth,
+
         };
     },
     methods: {
@@ -159,6 +228,7 @@ export default {
                 password: [],
                 confirm_password: [],
             };
+
 
             if (!this.auth.name) {
                 this.errors.name.push("Name is required.");
@@ -200,7 +270,7 @@ export default {
                 localStorage.setItem("_token", res.token);
                 console.log(user, "USER DATA");
 
-                if(user.role_id == 3 ){
+                if (user.role_id == 3) {
                     this.route('/instructor-dashboard')
                 } else {
                     this.route('/student-dashboard')
